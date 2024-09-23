4f48ce04cf594e366168b95b10b73531c9d166ef
Pratik Kumar <pratikkumar@Pratiks-MacBook-Air.local>
2024-09-23 12:16:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import Header from './Components/Header/header'
+// import Home from './Components/Home/home'
+// import Footer from './Components/Footer/footer'
+
+function App() {
+  
+
+  return (
+    <>
+    {/* <Header />
+    <Home />
+    <Footer /> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Github/Github.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+
+function Github() {
+    const[data,setData]=useState([])
+    useEffect(()=>{
+        fetch('https://api.github.com/users/pratikkumarthakur')
+        .then(response=> response.json())
+        .then(data => {
+            console.log(data);
+            setData(data)
+        })
+    },[])
+  return (
+    <div className='text-center m-4 bg-gray-700 text-white text-3xl p-4'>Github Followers: {data.followers}
+    <img src={data.avatar_url} alt="Git Picture" width={300} />
+    
+    </div>
+  )
+}
+
+export default Github
